b9b696112a75cf0e35c432c8bfc603498320fcd5
bxzd <87742849+jmqian-zju@users.noreply.github.com>
2024-05-22 20:47:21 +0800

    Update lenet.py

--- examples/gradient_leakage_attacks/nn/lenet.py
+++ examples/gradient_leakage_attacks/nn/lenet.py
@@ -8,31 +8,65 @@ https://github.com/mit-han-lab/dlg
 import torch.nn as nn
 from plato.config import Config
 
-
 class Model(nn.Module):
     def __init__(self, num_classes=Config().parameters.model.num_classes):
-        super().__init__()
+        super(Model, self).__init__()
         act = nn.Sigmoid
-        if Config().data.datasource == "EMNIST":
+        # if Config().data.datasource == "EMNIST":
+        if Config().data.datasource == "FashionMNIST":
             in_channel = 1
             in_size = 588
         if Config().data.datasource.startswith("CIFAR"):
             in_channel = 3
             in_size = 768
-
-        self.body = nn.Sequential(
-            nn.Conv2d(in_channel, 12, kernel_size=5, padding=5 // 2, stride=2),
-            act(),
-            nn.Conv2d(12, 12, kernel_size=5, padding=5 // 2, stride=2),
-            act(),
-            nn.Conv2d(12, 12, kernel_size=5, padding=5 // 2, stride=1),
-            act(),
-        )
-        self.fc = nn.Sequential(nn.Linear(in_size, num_classes))
+        self.conv1 = nn.Conv2d(in_channel, 12, kernel_size=5, padding=5 // 2, stride=2)
+        self.act1 = act()
+        self.conv2 = nn.Conv2d(12, 12, kernel_size=5, padding=5 // 2, stride=2)
+        self.act2 = act()
+        self.conv3 = nn.Conv2d(12, 12, kernel_size=5, padding=5 // 2, stride=1)
+        self.act3 = act()
+        self.fc = nn.Linear(in_size, num_classes)
 
     def forward(self, x):
-        out = self.body(x)
+        fea_list = []
+        out = self.conv1(x)
+        out = self.act1(out)
+        fea_list.append(out)  # 第一个中间层的输出
+        out = self.conv2(out)
+        out = self.act2(out)
+        fea_list.append(out)  # 第二个中间层的输出
+        out = self.conv3(out)
+        out = self.act3(out)
+        fea_list.append(out)  # 第三个中间层的输出
+
         feature = out.view(out.size(0), -1)
         out = self.fc(feature)
+        return out, feature, fea_list
 
-        return out, feature
+# class Model(nn.Module):
+#     def __init__(self, num_classes=Config().parameters.model.num_classes):
+#         super().__init__()
+#         act = nn.Sigmoid
+#         if Config().data.datasource == "EMNIST":
+#             in_channel = 1
+#             in_size = 588
+#         if Config().data.datasource.startswith("CIFAR"):
+#             in_channel = 3
+#             in_size = 768
+#
+#         self.body = nn.Sequential(
+#             nn.Conv2d(in_channel, 12, kernel_size=5, padding=5 // 2, stride=2),
+#             act(),
+#             nn.Conv2d(12, 12, kernel_size=5, padding=5 // 2, stride=2),
+#             act(),
+#             nn.Conv2d(12, 12, kernel_size=5, padding=5 // 2, stride=1),
+#             act(),
+#         )
+#         self.fc = nn.Sequential(nn.Linear(in_size, num_classes))
+#
+#     def forward(self, x):
+#         out = self.body(x)
+#         feature = out.view(out.size(0), -1)
+#         out = self.fc(feature)
+#
+#         return out, feature
