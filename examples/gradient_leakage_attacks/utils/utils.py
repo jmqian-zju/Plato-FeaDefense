be89e5874b54d45ec5c3937e04dcc03386768d1d
bxzd <87742849+jmqian-zju@users.noreply.github.com>
2024-05-22 20:46:53 +0800

    Update utils.py

--- examples/gradient_leakage_attacks/utils/utils.py
+++ examples/gradient_leakage_attacks/utils/utils.py
@@ -1,6 +1,6 @@
 import torch
 import torch.nn.functional as F
-
+import numpy as np
 
 def label_to_onehot(target, num_classes=10):
     target = torch.unsqueeze(target, 1)
@@ -18,3 +18,23 @@ def total_variation(x):
     dx = torch.mean(torch.abs(x[:, :, :, :-1] - x[:, :, :, 1:]))
     dy = torch.mean(torch.abs(x[:, :, :-1, :] - x[:, :, 1:, :]))
     return dx + dy
+
+# 新增代码
+
+def random_sparsify(image, sparsity):
+    # mask = (torch.rand_like(image[0]) > sparsity).unsqueeze(0).repeat(3, 1, 1)
+    mask = (torch.rand_like(image[0]) > sparsity)
+    # print("image:",image.shape)
+    # print("mask:",mask.shape)
+    return mask
+
+def random_sparsify_channels(image, sparsity):
+    mask = (torch.rand_like(image) > sparsity).float()
+    return image * mask
+
+def cosine_similarity(vector1, vector2):
+    dot_product = np.dot(vector1, vector2)
+    norm_vector1 = np.linalg.norm(vector1)
+    norm_vector2 = np.linalg.norm(vector2)
+    cosine_similarity = dot_product / (norm_vector1 * norm_vector2)
+    return cosine_similarity
