2311d1d522e08ce5b8ae92e1dd7540ee0be96edf
bxzd <87742849+jmqian-zju@users.noreply.github.com>
2024-05-22 20:46:07 +0800

    Update dlg_trainer.py

--- examples/gradient_leakage_attacks/dlg_trainer.py
+++ examples/gradient_leakage_attacks/dlg_trainer.py
@@ -13,6 +13,22 @@ from defense.GradDefense.sensitivity import compute_sens
 from defense.Outpost.perturb import compute_risk
 from utils.utils import cross_entropy_for_onehot, label_to_onehot
 
+from utils.utils import random_sparsify, random_sparsify_channels, cosine_similarity
+from skimage.metrics import peak_signal_noise_ratio as psnr
+from skimage.metrics import structural_similarity as ssim
+import cv2
+import matplotlib.pyplot as plt
+import torchvision.transforms.functional as TF
+import torch.nn.functional as F
+
+# 设置随机种子
+# seed = 0
+# np.random.seed(seed)
+# torch.manual_seed(seed)
+# torch.cuda.manual_seed(seed)
+# torch.backends.cudnn.deterministic = True
+
+
 criterion = cross_entropy_for_onehot
 tt = transforms.ToPILImage()
 
@@ -52,6 +68,7 @@ class Trainer(basic.Trainer):
         self.list_grad = None
         self.target_grad = None
         self.feature_fc1_graph = None
+        self.fea_graph_list = None
         self.sensitivity = None
 
     def get_train_loader(self, batch_size, trainset, sampler, **kwargs):
@@ -88,6 +105,9 @@ class Trainer(basic.Trainer):
             self.full_onehot_labels = label_to_onehot(
                 self.full_labels, num_classes=Config().parameters.model.num_classes
             )
+        # 新增代码
+        # print(examples)
+        # print(examples.shape)
 
         examples.requires_grad = True
         self.examples = examples
@@ -124,9 +144,30 @@ class Trainer(basic.Trainer):
                 self.list_grad.append(list((_.detach().clone() for _ in grad)))
         else:
             try:
-                outputs, self.feature_fc1_graph = self.model(examples)
+                # 新增代码
+                if ( hasattr(Config().algorithm, "defense")
+                    and Config().algorithm.defense == "MaskDefense"
+                ):
+                    mask_examples = examples.detach().clone()
+                    for i, mask_example in enumerate(mask_examples):
+                        mask = random_sparsify(mask_example, Config().algorithm.sparsity)
+                        mask_examples[i] = mask_examples[i] * mask
+                        # plt.imshow(TF.to_pil_image(mask_examples[i].cpu()))
+                        # plt.show()
+                    outputs, self.feature_fc1_graph, self.fea_graph_list = self.model(mask_examples)
+                else:
+                    outputs, self.feature_fc1_graph, self.fea_graph_list = self.model(examples)
             except:
+                # 新增代码
+                if ( hasattr(Config().algorithm, "defense")
+                    and Config().algorithm.defense == "MaskDefense"
+                ):
+                    for i, example in enumerate(examples):
+                        examples[i] = examples[i] * mask
+                print("第2步"*3)
+
                 outputs = self.model(examples)
+
             # Save the ground truth and gradients
             loss = self._loss_criterion(outputs, labels)
             grad = torch.autograd.grad(
@@ -168,7 +209,7 @@ class Trainer(basic.Trainer):
                 for f in range(deviation_f1_x_norm.size(1)):
                     deviation_f1_target[:, f] = 1
                     self.feature_fc1_graph.backward(
-                        deviation_f1_target, retain_graph=True
+                        deviation_f1_target, retain_graph=True,
                     )
                     deviation_f1_x = self.examples.grad.data
                     deviation_f1_x_norm[:, f] = torch.norm(
@@ -191,7 +232,7 @@ class Trainer(basic.Trainer):
                     self.device
                 )
 
-            elif Config().algorithm.defense == "GC":
+            elif Config().algorithm.defense == "GC" or Config().algorithm.defense == "MaskDefense":
                 for i, grad in enumerate(self.list_grad):
                     grad_tensor = grad.cpu().numpy()
                     flattened_weights = np.abs(grad_tensor.flatten())
@@ -221,6 +262,108 @@ class Trainer(basic.Trainer):
                     from defense.Outpost.perturb import noise
 
                     self.list_grad = noise(dy_dx=self.list_grad, risk=risk)
+
+            elif Config().algorithm.defense == "FeaDefense":
+                print("the length of list_grad:",len(self.list_grad))
+                for grad in self.list_grad:
+                    print(grad.shape)
+                img = self.examples.detach().cpu()
+                if Config().data.datasource == "CIFAR10":
+                    img = (0.299 * img[:, 0, :, :] + 0.587 * img[:, 1, :, :] + 0.114 * img[:, 2, :, :]).unsqueeze(1)
+                fea_list = [fea.detach().cpu() for fea in self.fea_graph_list]
+                num_layers = len(fea_list)
+                cos_value_matrix_3d = []
+                for i in range(num_layers):
+                    fea_graph = fea_list[i]
+                    num_samples = fea_graph.size(0)
+                    num_channels = fea_graph.size(1)
+                    cos_value_matrix_2d = []
+                    for j in range(num_samples):
+                        avg_cos = 0
+                        cos_value_list = []
+                        for k in range(num_channels):
+                            o_fea = img[j, 0].numpy()
+                            fea = fea_graph[j, k].numpy()
+                            fea = cv2.resize(fea, (o_fea.shape[0], o_fea.shape[1]))
+                            cos_value = cosine_similarity(o_fea.ravel(), fea.ravel())
+                            avg_cos += cos_value
+                            cos_value_list.append(cos_value)
+                        cos_value_matrix_2d.append(cos_value_list)
+                    cos_value_matrix_3d.append(cos_value_matrix_2d)
+                    cos_value_matrix_3d.append(cos_value_matrix_2d)
+                cos_value_matrix = np.mean(cos_value_matrix_3d, axis=1)
+                # print(cos_value_matrix.shape)
+
+                for i, grad in enumerate(self.list_grad):
+                    grad_tensor = grad.cpu().numpy()
+                    if i >= 6 : continue # 忽略FC层
+                    for j in range(grad_tensor.shape[0]):
+                        noise = np.random.normal(0, cos_value_matrix[i][j] * Config().algorithm.sigma, size=grad_tensor[j].shape)
+                        grad_tensor[j] = grad_tensor[j] + noise
+                    self.list_grad[i] = torch.Tensor(grad_tensor).to(self.device)
+
+                # TODO batch img [batch, 3, 32, 32]
+                # img = self.examples.detach().cpu()
+                # # print("origin img:",img.shape)
+                # if Config().data.datasource == "CIFAR10":
+                #     img = (0.299 * img[:, 0, :, :] + 0.587 * img[:, 1, :, :] + 0.114 * img[:, 2, :, :]).unsqueeze(1)
+                # # print("origin img gray:",img.shape)
+                # fea_list = [fea.detach().cpu() for fea in self.fea_graph_list]
+                # # torch.Size([1, 12, 16, 16])
+                # # torch.Size([1, 12, 8, 8])
+                # # torch.Size([1, 12, 8, 8])
+                #
+                # num_layers = len(fea_list)
+                # pool_img = img
+                # fig, axes = plt.subplots(num_layers, 12, figsize=(4 * 12, 4 * num_layers))
+                # cos_value_matrix = []
+                # for i in range(num_layers):
+                #     fea_graph = fea_list[i]
+                #     num_channels = fea_graph.size(1)
+                #     # print(f"conv {i+1} channels:",num_channels)
+                #     # if i == 0 or i == 1:
+                #     #     pool_img = F.avg_pool2d(img, kernel_size=2 * (i + 1), stride=2 * (i + 1))
+                #     avg_psnr = 0
+                #     avg_ssim = 0
+                #     avg_cos = 0
+                #     cos_value_list = []
+                #     # print("pool_img shape:",pool_img.shape)
+                #     for j in range(num_channels):
+                #         o_fea = pool_img[0, 0].numpy()
+                #         fea = fea_graph[0, j].numpy()
+                #         fea = cv2.resize(fea, (o_fea.shape[0], o_fea.shape[1]))
+                #
+                #         psnr_value = psnr(o_fea, fea, data_range=1.0)
+                #         ssim_value = ssim(o_fea, fea, data_range=1.0)
+                #         cos_value = cosine_similarity(o_fea.ravel(), fea.ravel())
+                #         avg_psnr += psnr_value
+                #         avg_ssim += ssim_value
+                #         avg_cos += cos_value
+                #         cos_value_list.append(cos_value)
+                #
+                #         axes[i, j].imshow(fea)
+                #         axes[i, j].set_title(
+                #             f'Conv {i} Channel {j + 1}\n psnr {psnr_value:.4f}\n ssim {ssim_value:.4f}\n cos {cos_value:.4f}',
+                #             fontsize=5)
+                #         axes[i, j].axis('off')
+                #
+                #     cos_value_matrix.append(cos_value_list)
+                #     cos_value_matrix.append(cos_value_list)
+                #     # print("avg_psnr:", avg_psnr)
+                #     # print("avg_ssim:", avg_ssim)
+                #     # print("avg_cos:", avg_cos)
+                # plt.tight_layout()
+                # plt.show()
+                # # print(len(cos_value_matrix))
+                # # print(cos_value_matrix)
+                #
+                # for i, grad in enumerate(self.list_grad):
+                #     grad_tensor = grad.cpu().numpy()
+                #     if i >= 6 : continue # 忽略FC层
+                #     for j in range(grad_tensor.shape[0]):
+                #         noise = np.random.normal(0, cos_value_matrix[i][j] * Config().algorithm.sigma, size=grad_tensor[j].shape)
+                #         grad_tensor[j] = grad_tensor[j] + noise
+                #     self.list_grad[i] = torch.Tensor(grad_tensor).to(self.device)
 
             # cast grad back to tuple type
             grad = tuple(self.list_grad)
